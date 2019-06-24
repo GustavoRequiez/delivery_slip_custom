6a866d84dfb20653ff915d31eb9c9c10377d735a
Gustavo Flores <gustavofc65@gmail.com>
2019-06-24 11:25:51 -0500

    delivery slip custom report

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "delivery_slip_report",
+    'summary': """
+        Delivery Slip Report Custom""",
+    'description': """
+        Delivery Slip Report Custom
+    """,
+    'author': "gflores",
+    'website': "https://www.gruporequiez.com",
+    'category': 'Sales',
+    'version': '0.0.1',
+    'depends': ['sale'],
+    'data': [
+        # views
+        # reports
+        'report/delivery_slip_custom.xml',
+        # data
+    ],
+    'demo': [
+    ],
+}
